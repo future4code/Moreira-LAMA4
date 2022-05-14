cd0e7cabaaebabd9d6e56b061b766ad94e4e9a8a
Leona Christen <leo.christenmacedo@gmail.com>
2022-05-13 21:42:15 -0300

    Get band details function done

--- src/controller/BandController.ts
+++ src/controller/BandController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { BandBusiness } from "../business/BandBusiness";
+import { GetBandDetailsDTO } from "../types/DTO/GetBandDetailsDTO";
 import { signBandInputDTO } from "../types/DTO/signBandInputDTO";
 
 export class BandController {
@@ -38,6 +39,35 @@ export class BandController {
                     res.status(500).send('Erro por parte do servidor');
                     break;
             }    
+        }
+    };
+
+    public getDetails = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const name: string = req.query.name as string;
+            const id: string = req.query.id as string;
+            const input: GetBandDetailsDTO = {name, id};
+
+            const band = await this.bandBusiness.getDetails(input);
+
+            res.status(200).send(band);
+        } 
+        catch (error: any) {
+            switch (error.message) {
+                case 'Nome e/ou id da banda devem ser informados':
+                    res.status(400).send(error.message);
+                    break;
+                case 'Não foi possivel encontrar a banda, verifique se o nome e/ou id foram inseridos na query':
+                    res.status(404).send(error.message);
+                    break;
+                case 'Erro na conexão com o banco de dados':
+                    res.status(500).send(error.message);
+                    break;
+            
+                default:
+                    res.status(500).send('Erro por parte do servidor');
+                    break;
+            }                
         }
     };
 };
